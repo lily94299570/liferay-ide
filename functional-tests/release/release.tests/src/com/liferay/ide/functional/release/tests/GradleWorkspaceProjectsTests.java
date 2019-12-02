51eda656701a77170c311804746f37855aee473f
Lily Li <lily.li@liferay.com>
2019-12-02 13:33:54 +0800

    create gradle workspace for release

--- functional-tests/release/release.tests/src/com/liferay/ide/functional/release/tests/GradleWorkspaceProjectsTests.java
+++ functional-tests/release/release.tests/src/com/liferay/ide/functional/release/tests/GradleWorkspaceProjectsTests.java
@@ -14,11 +14,363 @@
 
 package com.liferay.ide.functional.release.tests;
 
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Ignore;
+import org.junit.Rule;
+import org.junit.Test;
+
+import com.liferay.ide.functional.liferay.SwtbotBase;
+import com.liferay.ide.functional.liferay.support.project.ProjectSupport;
+import com.liferay.ide.functional.liferay.support.workspace.LiferayWorkspaceGradle72Support;
+
 /**
  * @author Ashley Yuan
  */
-public class GradleWorkspaceProjectsTests {
-	
-	//create all projects in liferay gradle workspace
+public class GradleWorkspaceProjectsTests extends SwtbotBase {
 
+	@ClassRule
+	public static LiferayWorkspaceGradle72Support liferayWorkspace = new LiferayWorkspaceGradle72Support(bot);
+
+	//create all projects in liferay gradle workspace
+	@Test
+	public void createApi() {
+		wizardAction.openNewLiferayModuleWizard();
+
+		wizardAction.newModule.prepareGradle(project.getName(), API);
+
+		wizardAction.finish();
+
+		jobAction.waitForNoRunningJobs();
+
+		Assert.assertTrue(viewAction.project.visibleFileTry(project.getName()));
+	}
+
+	@Test
+	public void createControlMenuEntry() {
+		wizardAction.openNewLiferayModuleWizard();
+
+		wizardAction.newModule.prepareGradle(project.getName(), CONTROL_MENU_ENTRY);
+
+		wizardAction.finish();
+
+		jobAction.waitForNoRunningJobs();
+
+		Assert.assertTrue(viewAction.project.visibleFileTry(project.getName()));
+	}
+
+	@Ignore("ignore because the build process takes too long")
+	@Test
+	public void createFormField() {
+		wizardAction.openNewLiferayModuleWizard();
+
+		wizardAction.newModule.prepareGradle(project.getName(), FORM_FIELD);
+
+		wizardAction.finish();
+
+		jobAction.waitForNoRunningJobs();
+
+		Assert.assertTrue(viewAction.project.visibleFileTry(project.getName()));
+	}
+
+	@Test
+	public void createLayoutTemplate() {
+		wizardAction.openNewLiferayModuleWizard();
+
+		wizardAction.newModule.prepareGradle(project.getName(), LAYOUT_TEMPLATE);
+
+		wizardAction.finish();
+
+		jobAction.waitForNoRunningJobs();
+
+		Assert.assertTrue(viewAction.project.visibleFileTry(project.getName()));
+	}
+
+	@Test
+	public void createMvcPortlet() {
+		wizardAction.openNewLiferayModuleWizard();
+
+		wizardAction.newModule.prepareGradle(project.getName(), MVC_PORTLET);
+
+		wizardAction.finish();
+
+		jobAction.waitForNoRunningJobs();
+
+		Assert.assertTrue(viewAction.project.visibleFileTry(project.getName()));
+	}
+
+	@Ignore("ignore because the build process takes too long")
+	@Test
+	public void createNpmAngularPortlet() {
+		wizardAction.openNewLiferayModuleWizard();
+
+		wizardAction.newModule.prepareGradle(project.getName(), NPM_ANGULAR_PORTLET);
+
+		wizardAction.finish();
+
+		jobAction.waitForNoRunningJobs();
+
+		Assert.assertTrue(viewAction.project.visibleFileTry(project.getName()));
+	}
+
+	@Ignore("ignore because the build process takes too long")
+	@Test
+	public void createNpmReactPortlet() {
+		wizardAction.openNewLiferayModuleWizard();
+
+		wizardAction.newModule.prepareGradle(project.getName(), NPM_REACT_PORTLET);
+
+		wizardAction.finish();
+
+		jobAction.waitForNoRunningJobs();
+
+		Assert.assertTrue(viewAction.project.visibleFileTry(project.getName()));
+	}
+
+	@Ignore("ignore because the build process takes too long")
+	@Test
+	public void createNpmVuejsPortlet() {
+		wizardAction.openNewLiferayModuleWizard();
+
+		wizardAction.newModule.prepareGradle(project.getName(), NPM_VUEJS_PORTLET);
+
+		wizardAction.finish();
+
+		jobAction.waitForNoRunningJobs();
+
+		Assert.assertTrue(viewAction.project.visibleFileTry(project.getName()));
+	}
+
+	@Test
+	public void createPanelApp() {
+		wizardAction.openNewLiferayModuleWizard();
+
+		wizardAction.newModule.prepareGradle(project.getName(), PANEL_APP);
+
+		wizardAction.finish();
+
+		jobAction.waitForNoRunningJobs();
+
+		Assert.assertTrue(viewAction.project.visibleFileTry(project.getName()));
+	}
+
+	@Test
+	public void createPortletConfigurationIcon() {
+		wizardAction.openNewLiferayModuleWizard();
+
+		wizardAction.newModule.prepareGradle(project.getName(), PORTLET_CONFIGURATION_ICON);
+
+		wizardAction.finish();
+
+		jobAction.waitForNoRunningJobs();
+
+		Assert.assertTrue(viewAction.project.visibleFileTry(project.getName()));
+	}
+
+	@Test
+	public void createPortletProvider() {
+		wizardAction.openNewLiferayModuleWizard();
+
+		wizardAction.newModule.prepareGradle(project.getName(), PORTLET_PROVIDER);
+
+		wizardAction.finish();
+
+		jobAction.waitForNoRunningJobs();
+
+		Assert.assertTrue(viewAction.project.visibleFileTry(project.getName()));
+	}
+
+	@Test
+	public void createPortletToolbarContributor() {
+		wizardAction.openNewLiferayModuleWizard();
+
+		wizardAction.newModule.prepareGradle(project.getName(), PORTLET_PROVIDER);
+
+		wizardAction.finish();
+
+		jobAction.waitForNoRunningJobs();
+
+		Assert.assertTrue(viewAction.project.visibleFileTry(project.getName()));
+	}
+
+	@Test
+	public void createRest() {
+		wizardAction.openNewLiferayModuleWizard();
+
+		wizardAction.newModule.prepareGradle(project.getName(), REST);
+
+		wizardAction.finish();
+
+		jobAction.waitForNoRunningJobs();
+
+		Assert.assertTrue(viewAction.project.visibleFileTry(project.getName()));
+	}
+
+	@Test
+	public void createService() {
+		wizardAction.openNewLiferayModuleWizard();
+
+		wizardAction.newModule.prepareGradle(project.getName(), SERVICE);
+
+		wizardAction.next();
+
+		wizardAction.newModuleInfo.openSelectServiceDialog();
+
+		dialogAction.prepareText("*lifecycleAction");
+
+		dialogAction.confirm();
+
+		wizardAction.finish();
+
+		jobAction.waitForNoRunningJobs();
+
+		Assert.assertTrue(viewAction.project.visibleFileTry(project.getName()));
+
+		viewAction.project.openFile(
+			project.getName(), "src/main/java", project.getName(), project.getCapitalName() + ".java");
+
+		viewAction.project.implementMethods(
+			project.getName(), "src/main/java", project.getName(), project.getCapitalName() + ".java");
+
+		dialogAction.confirm();
+
+		editorAction.save();
+
+		editorAction.close();
+	}
+
+	@Test
+	public void createServiceBuilder() {
+		wizardAction.openNewLiferayModuleWizard();
+
+		wizardAction.newModule.prepareGradle(project.getName(), SERVICE_BUILDER);
+
+		wizardAction.finish();
+
+		jobAction.waitForNoRunningJobs();
+
+		Assert.assertTrue(viewAction.project.visibleFileTry(project.getName()));
+	}
+
+	@Test
+	public void createServiceWrapper() {
+		wizardAction.openNewLiferayModuleWizard();
+
+		wizardAction.newModule.prepareGradle(project.getName(), SERVICE_WRAPPER);
+
+		wizardAction.next();
+
+		wizardAction.newModuleInfo.openSelectServiceDialog();
+
+		dialogAction.prepareText("*AccountLocalServiceWrapper");
+
+		dialogAction.confirm();
+
+		wizardAction.finish();
+
+		jobAction.waitForNoRunningJobs();
+
+		Assert.assertTrue(viewAction.project.visibleFileTry(project.getName()));
+	}
+
+	@Test
+	public void createSimulationPanelEntry() {
+		wizardAction.openNewLiferayModuleWizard();
+
+		wizardAction.newModule.prepareGradle(project.getName(), SIMULATION_PANEL_ENTRY);
+
+		wizardAction.finish();
+
+		jobAction.waitForNoRunningJobs();
+
+		Assert.assertTrue(viewAction.project.visibleFileTry(project.getName()));
+	}
+
+	@Test
+	public void createTemplateContextContributor() {
+		wizardAction.openNewLiferayModuleWizard();
+
+		wizardAction.newModule.prepareGradle(project.getName(), TEMPLATE_CONTEXT_CONCONTRIBUTOR);
+
+		wizardAction.finish();
+
+		jobAction.waitForNoRunningJobs();
+
+		Assert.assertTrue(viewAction.project.visibleFileTry(project.getName()));
+	}
+
+	@Test
+	public void createTheme() {
+		wizardAction.openNewLiferayModuleWizard();
+
+		wizardAction.newModule.prepareGradle(project.getName(), THEME);
+
+		wizardAction.finish();
+
+		jobAction.waitForNoRunningJobs();
+
+		Assert.assertTrue(viewAction.project.visibleFileTry(project.getName()));
+
+		viewAction.project.openFile(project.getName(), "src", "main", "webapp", "css", "_custom.scss");
+
+		editorAction.setText("#wrapper{ background:yellow; }");
+
+		editorAction.close();
+	}
+
+	@Test
+	public void createThemeContributor() {
+		wizardAction.openNewLiferayModuleWizard();
+
+		wizardAction.newModule.prepareGradle(project.getName(), THEME_CONTRIBUTOR);
+
+		wizardAction.finish();
+
+		jobAction.waitForNoRunningJobs();
+
+		Assert.assertTrue(viewAction.project.visibleFileTry(project.getName()));
+	}
+
+	@Ignore("ignore by IDE-4828")
+	@Test
+	public void createWarCoreExt() {
+		wizardAction.openNewLiferayModuleWizard();
+
+		wizardAction.newModule.prepareGradle(project.getName(), WAR_CORE_EXT);
+
+		wizardAction.finish();
+
+		jobAction.waitForNoRunningJobs();
+
+		Assert.assertTrue(viewAction.project.visibleFileTry(project.getName()));
+	}
+
+	@Test
+	public void createWarHook() {
+		wizardAction.openNewLiferayModuleWizard();
+
+		wizardAction.newModule.prepareGradle(project.getName(), WAR_HOOK);
+
+		wizardAction.finish();
+
+		jobAction.waitForNoRunningJobs();
+
+		Assert.assertTrue(viewAction.project.visibleFileTry(project.getName()));
+	}
+
+	@Test
+	public void createWarMvcPortlet() {
+		wizardAction.openNewLiferayModuleWizard();
+
+		wizardAction.newModule.prepareGradle(project.getName(), WAR_MVC_PORTLET);
+
+		wizardAction.finish();
+
+		jobAction.waitForNoRunningJobs();
+
+		Assert.assertTrue(viewAction.project.visibleFileTry(project.getName()));
+	}
+
+	@Rule
+	public ProjectSupport project = new ProjectSupport(bot);
 }
